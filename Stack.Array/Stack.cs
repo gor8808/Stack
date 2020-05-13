1f8a8361cf3f8595339eb37434a3ae3b7301bf5f
Gor Grigoryan <gor8808@users.noreply.github.com>
2020-05-13 14:55:28 +0400

    Initial commit

---
+++ Stack.Array/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Stack.Array
+{
+    public class Stack<T> : IEquatable<T>
+    {
+        T[] _items = new T[0];
+        private int _size;
+
+        public void Push(T item)
+        {
+            if(_size == _items.Length)
+            {
+                int newLength = _size == 0 ? 4 : _size * 2;
+                T[] newItems = new T[newLength];
+                _items.CopyTo(newItems, 0);
+                _items = newItems;
+
+            }
+            _items[_size] = item;
+            _size++;
+        }
+        public T Pop()
+        {
+            if(_items.Length == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+            _size--;
+            return _items[_size];
+        }
+        public bool Equals(T other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack.LinkedList
+{
+    class Stack<T> : IEquatable<T>
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        public void Push(T item)
+        {
+            _list.AddFirst(item);
+        }
+        public T Pop()
+        {
+            T itemRec = _list.First.Value;
+            _list.RemoveFirst();
+            return itemRec;
+        }
+        public T Peak()
+        {
+            return _list.First.Value;
+        }
+        public bool Equals(T other)
+        {
+            throw new NotImplementedException();
+        }
+        public int Count
+        {
+            get
+            {
+                return _list.Count;
+            } 
+        }
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+    }
+}
